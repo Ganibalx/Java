0050b0c86101b0af2d85812d49f2f826d76c0af9
hitman111 <ganibal1993@gmail.com>
2023-02-26 21:40:07 +0500

    ДЗ 5

---
+++ seminari/05/p1.java
@@ -0,0 +1,30 @@
+public class p1 {
+    public static void main(String[] args) {
+    int[][] array = karta();
+    print(array);     
+    }
+
+    private static int[][] karta(){ 
+        int[][] array = {{-1,-1,-1,-1,-1,-1,-1},
+                         {-1, 0, 0, 0, 0, 0,-1},
+                         {-1, 1,-1, 0, 0,20,-1},
+                         {-1, 0,-1,-1,-1, 0,-1},
+                         {-1, 0,-1, 0,20, 0,-1},
+                         {-1, 0, 0, 0, 0, 0,-1},
+                         {-1,-1,-1,-1,-1,-1,-1}};
+        return array; 
+    }
+
+    private static void print(int[][] array){        
+        for(int[] i: array){
+            for(int j: i){                 
+                System.out.print(j+"\t");                
+            }
+            System.out.println();
+        }
+    }
+
+    static void poisk(int[][] array){
+        
+    }
+}
